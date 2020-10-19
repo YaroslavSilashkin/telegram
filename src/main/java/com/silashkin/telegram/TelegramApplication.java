95492b3b5fcb055b5e0b2a48eae9e40485bc64f1
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2020-10-20 00:04:24 +0300

    EnableScheduling

--- src/main/java/com/silashkin/telegram/TelegramApplication.java
+++ src/main/java/com/silashkin/telegram/TelegramApplication.java
@@ -2,9 +2,11 @@ package com.silashkin.telegram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramApplication {
 
     public static void main(String[] args) {
