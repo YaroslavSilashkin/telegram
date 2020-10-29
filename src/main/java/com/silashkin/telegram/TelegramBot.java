de8823de7bba2b6fe232ce1f21b490fc0789b15f
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2020-10-29 21:49:45 +0300

    path

--- src/main/java/com/silashkin/telegram/TelegramBot.java
+++ src/main/java/com/silashkin/telegram/TelegramBot.java
@@ -1,6 +1,5 @@
 package com.silashkin.telegram;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
@@ -9,14 +8,14 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-@Getter
 @Component
 public class TelegramBot extends TelegramWebhookBot {
 
-    private static final String BOT_TOKEN = "1152592908:AAEiPxwMQMV0Evvy8o48u_dZxNzPMArxMKE";
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
     @Value("${username}")
     private static String BOT_USERNAME;
-    private static final String BOT_PATH = "https://telegrambotnn.herokuapp.com/";
+    @Value("${path}")
+    private static String BOT_PATH;
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
         if (update.getMessage() != null && update.getMessage().hasText()) {
