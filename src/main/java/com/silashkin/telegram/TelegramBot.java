8fe9ed40fe8a57d9a65ea9df7cdc87daaf597dfa
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2020-10-29 18:12:31 +0300

    commit

--- src/main/java/com/silashkin/telegram/TelegramBot.java
+++ src/main/java/com/silashkin/telegram/TelegramBot.java
@@ -1,5 +1,6 @@
 package com.silashkin.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -11,6 +12,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class TelegramBot extends TelegramWebhookBot {
 
     private static final String BOT_TOKEN = "1152592908:AAEiPxwMQMV0Evvy8o48u_dZxNzPMArxMKE";
+    @Value("${username}")
     private static final String BOT_USERNAME = "@testninobot";
     private static final String BOT_PATH = "https://telegrambotnn.herokuapp.com/";
 
@@ -18,7 +20,7 @@ public class TelegramBot extends TelegramWebhookBot {
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
         if (update.getMessage() != null && update.getMessage().hasText()) {
             try {
-                execute(new SendMessage(update.getMessage().getChatId(), "test"));
+                execute(new SendMessage(update.getMessage().getChatId(), update.getMessage().getText()));
             } catch (TelegramApiException ex) {
             }
         }
