7645db1398f6b07336abac71c308dd149b6165af
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2020-11-09 23:19:38 +0300

    commit

--- src/main/java/com/silashkin/telegram/Ping.java
+++ src/main/java/com/silashkin/telegram/Ping.java
@@ -23,7 +23,7 @@ public class Ping {
             URL url = new URL(getUrl());
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
-            System.out.println(connection.getResponseCode() + "test");
+            System.out.println(connection.getResponseCode());
             connection.disconnect();
         } catch (IOException e) {
         }
