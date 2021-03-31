b78784ba4af3bfcf2e0293ae4c949abbc2b37b9f
YaroslavSilashkin <poizzzennn@mail.ru>
2021-03-31 23:59:38 +0300

    any

--- src/main/java/com/silashkin/telegram/repository/PostgresRepository.java
+++ src/main/java/com/silashkin/telegram/repository/PostgresRepository.java
@@ -1,6 +1,9 @@
 package com.silashkin.telegram.repository;
 
+import com.silashkin.telegram.cache.UserCache;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostgresRepository extends JpaRepository {
+public interface PostgresRepository<String, UserCache> extends JpaRepository<String, UserCache> {
+
+    public String ByName();
 }
