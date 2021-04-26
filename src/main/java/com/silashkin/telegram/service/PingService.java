13bc1ed15c8381ea281b023751014138729f2eab
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2021-04-26 23:37:14 +0300

    any

--- src/main/java/com/silashkin/telegram/service/PingService.java
+++ src/main/java/com/silashkin/telegram/service/PingService.java
@@ -1,17 +1,15 @@
 package com.silashkin.telegram.service;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.scheduling.annotation.Scheduled;
+import java.io.IOException;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @Getter
-@Setter
 public class PingService {
 
     @Value("${path}")
