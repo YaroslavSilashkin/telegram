4cdc9db51900e7ade358a4af521222a6bbb30d39
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2021-04-26 23:27:54 +0300

    any

--- src/main/java/com/silashkin/telegram/service/PingService.java
+++ src/main/java/com/silashkin/telegram/service/PingService.java
@@ -26,6 +26,7 @@ public class PingService {
             System.out.println(connection.getResponseCode());
             connection.disconnect();
         } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
