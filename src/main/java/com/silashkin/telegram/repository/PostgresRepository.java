57f5a4a97be7fde12661912dcd55905ec274b05d
YaroslavSilashkin <YaroslavSilashkin@users.noreply.github.com>
2021-05-05 10:58:28 +0300

    any

--- src/main/java/com/silashkin/telegram/repository/PostgresRepository.java
+++ src/main/java/com/silashkin/telegram/repository/PostgresRepository.java
@@ -1,9 +1,7 @@
 package com.silashkin.telegram.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostgresRepository<String, Users> extends JpaRepository<String, Users> {
 
     //Users ByName(String name);
